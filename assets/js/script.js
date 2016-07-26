4eec32a2ab2a3c09b14a3cc28fe6a867cad342c0
gunnu9 <kdeepikarao_9@yahoo.com>
2016-07-26 00:41:55 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
+    } );
+} );
